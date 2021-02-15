27879854e3ed606ca4529357d29aeac9533259bc
Kevin Mullins <46154511+pnotequalnp@users.noreply.github.com>
2021-02-15 03:45:12 -0800

    Convert to Nix Flake (#12)
    * Remove dead code
    
    This was made obsolete by my learning of the Paths_ module automatically
    provided by by Cabal
    
    * Add `hie.yaml`
    
    Makes HLS play a bit nicer with everything
    
    * Update `base` package version
    
    Necessary for the upcoming Nix changes.
    
    * Convert to flake
    
    * Update existing CI
    
    The GHC version was updated to match the Nixpkgs version used.
    
    * Add Nix CI
    
    * Add Docker release via Nix to CI
    
    * Update readme
    
    * Version bump 0.1.0.5 -> 0.1.0.6

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Discord bot for /r/Cubers";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      hs = pkgs.haskellPackages;
+      curve = hs.callCabal2nix "curve" ./. {};
+      docker = pkgs.dockerTools.buildImage {
+        name = "curve";
+        tag = "0.1.0.6";
+        contents = [ pkgs.bash pkgs.coreutils curve ];
+      };
+    in {
+      defaultPackage = curve;
+      packages = { inherit docker curve; };
+      devShell = curve.env.overrideAttrs (super: {
+        nativeBuildInputs = super.nativeBuildInputs ++ [
+          hs.cabal-install
+        ];
+      });
+    }
+  );
+}
