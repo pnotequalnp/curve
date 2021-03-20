b28f22b2a1197f745a487aa605f6aa2acda22a8b
Kevin Mullins <kevin@pnotequalnp.com>
2021-03-20 13:18:14 -0700

    Change to only Linux

--- flake.nix
+++ flake.nix
@@ -5,24 +5,24 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        hs = pkgs.haskellPackages;
-        curve = hs.callCabal2nix "curve" ./. { };
-        docker = pkgs.dockerTools.buildImage {
-          name = "pnotequalnp/curve";
-          tag = "0.1.0.8";
-          contents = [ pkgs.bash pkgs.coreutils curve ];
-          config.Cmd = [ "curve" ];
-        };
-      in rec {
-        defaultPackage = curve;
-        packages = { inherit docker curve; };
-        nixosModule = nixosModules.curve;
-        nixosModules = { curve = import ./module.nix curve; };
-        devShell = curve.env.overrideAttrs (super: {
-          nativeBuildInputs = super.nativeBuildInputs ++ [ hs.cabal-install ];
-        });
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      hs = pkgs.haskellPackages;
+      curve = hs.callCabal2nix "curve" ./. { };
+      docker = pkgs.dockerTools.buildImage {
+        name = "pnotequalnp/curve";
+        tag = "0.1.0.8";
+        contents = [ pkgs.bash pkgs.coreutils curve ];
+        config.Cmd = [ "curve" ];
+      };
+      module = import ./module.nix curve;
+    in rec {
+      defaultPackage.x86_64-linux = curve;
+      packages.x86_64-linux = { inherit docker curve; };
+      nixosModule.x86_64-linux = module;
+      nixosModules.x86_64-linux = { curve = module; };
+      devShell.x86_64-linux = curve.env.overrideAttrs (super: {
+        nativeBuildInputs = super.nativeBuildInputs ++ [ hs.cabal-install ];
       });
+    };
 }
